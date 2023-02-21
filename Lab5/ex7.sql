ab1434deca8cae4a22c6766cfbd29c2c4ce6029b
Ernest <58702017+ErnestMatskevich@users.noreply.github.com>
2023-02-21 16:30:13 +0300

    Update ex7.sql

--- Lab5/ex7.sql
+++ Lab5/ex7.sql
@@ -1,1 +1,7 @@
-SELECT * FROM section where semester='Autumn'
+SELECT MAX(COALESCE(enrollment, 0)) AS max_enrollment
+FROM (
+   SELECT COUNT(*) AS enrollment
+   FROM takes
+  WHERE year=2022 AND semester='Fall'
+   GROUP BY course_id, sec_id, semester, year
+) AS enrollment
